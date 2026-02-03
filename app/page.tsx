76af54a34f1de01b00ab579ddf24d893281546d4
Priyanshkashyap <kpriyansh2121@gmail.com>
2026-02-03 19:24:21 +0530

    added constants and eventcard to display events

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,20 @@
+import EventCard from "@/components/EventCard";
 import ExploreBtn from "@/components/ExploreBtn";
-import Navbar from "@/components/Navbar";
+import { events } from "@/lib/constants";
 export default function Home() {
   console.log("hi");
   return (
     <section>
-      <Navbar></Navbar>
       <h1 className="text-center"> The Hub For Every Dev <br /> You Can't Miss</h1>
       <p className="text-center mt-5"> Hackathons,Meetups and Conferences all in one place</p>
       <ExploreBtn />
       <div className="mt-20 space-y-7"> 
         <h3>Featured Events</h3>
         <ul className="events">{
-            [1,2,3,4,5].map((even)=>( // map function me curly braces ke jgh we give normal braces to automatically use this function
-              <li key={even}> Event {even} </li>
+            events.map((even)=>( // map function me curly braces ke jgh we give normal braces to automatically use this function. also ...even is basically sending all properties of events to the function as props
+              <li key={even.title}>
+                <EventCard {...even} />  
+              </li>
             ))
           }
         </ul>
